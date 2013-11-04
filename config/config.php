74fce5fcae96f4ad47a68fd0c96079704ce2a12b
Robert Coscia <coscia@g.harvard.edu>
2013-11-04 14:58:31 -0500

    Set the new db name
    Set the new DB name
    
    Signed-off-by: Robert Coscia <coscia@g.harvard.edu>

--- config/config.php
+++ config/config.php
@@ -31,7 +31,7 @@ A email designated to receive messages from the server. Examples:
 	define('SYSTEM_EMAIL', 'coscia@g.harvard.edu'); 
 
 # Default DB name for this app
-	define('DB_NAME', 'p2_robcoscia_biz'); 
+	define('DB_NAME', 'robcosci_p2_robcoscia_biz'); 
 
 # Timezone
 	define('TIMEZONE', 'America/New_York');
