371a2edad4ae50ab1594d594f19051995d627854
Robert Coscia <coscia@g.harvard.edu>
2013-11-04 14:48:21 -0500

    Set email address
    Set email address
    
    Signed-off-by: Robert Coscia <coscia@g.harvard.edu>

--- config/config.php
+++ config/config.php
@@ -28,7 +28,7 @@ A email designated to receive messages from the server. Examples:
  	
  	Some might want this to be the same as the APP_EMAIL, others might want to create a designated gmail address for it
 */ 	
-	define('SYSTEM_EMAIL', 'webmaster@myapp.com'); 
+	define('SYSTEM_EMAIL', 'coscia@g.harvard.edu'); 
 
 # Default DB name for this app
 	define('DB_NAME', 'p2_robcoscia_biz'); 
