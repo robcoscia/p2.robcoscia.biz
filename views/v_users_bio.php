f203f5452223c4a76bfb2393391d28ec498a312d
Robert Coscia <coscia@g.harvard.edu>
2013-11-06 12:12:00 -0500

    improved bio page layout
    improved bio page layout
    
    Signed-off-by: Robert Coscia <coscia@g.harvard.edu>

--- views/v_users_bio.php
+++ views/v_users_bio.php
@@ -11,9 +11,19 @@
 	<br>
 	<br>
 	<br>
-	<?php if(isset($users[0]['biography'])): ?>
-		<p><?php echo $users[0]['biography'] ?></p>
-	<?php else: ?>
-		<p> Ask me for my bio. <p>
-	<?php endif; ?>
+	<p><b>Location:</b> <?php echo $users[0]['location'] ?> </p>
+	<br>
+	<br>
+	<br>
+	<br>
+	<p><b>Biography:</b>
+	<div id="BioContentDiv">
+		<?php if(isset($users[0]['biography'])): ?>
+			<p><?php echo $users[0]['biography'] ?></p>
+		<?php else: ?>
+			<p> Ask me for my bio. </p>
+		<?php endif; ?>
+		
+	</div>
+			
 </div>
